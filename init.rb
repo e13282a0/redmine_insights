0179e290e1df2a4e2fdbb96ecfb8cb3113db7bba
Christian <christian.rust@gmx.com>
2021-11-29 18:01:40 +0100

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_performance_charts do
+  name 'Redmine Performance Charts plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end

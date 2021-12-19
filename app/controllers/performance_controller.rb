6d4575fcd2b8761b0d35bb4dbdb548d8baca3357
Christian <christian.rust@gmx.com>
2021-12-19 09:50:58 +0100

    formatted data to frontend

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -13,27 +13,25 @@ class PerformanceController < ApplicationController
 
     #get involved users
     userIDs = timeEntries.pluck(:user_id).to_a()
-    @users = User.where(:id => userIDs).to_a()
+    users = User.where(:id => userIDs).to_a()
+    @users = users.map{ |user|  {'userID'=>user.id, "userName"=>user.lastname}}
 
-    #read all top level issues
+    #read all top level issues and related time entries
     topLevelIssues = Issue.where( :project_id => @project.id, :parent_id => nil)
-    logger.info "top level issues: "+topLevelIssues.count().to_s()
 
-    # combine top level issues and related time entries
-    @issues=[]
+    @time_entries=[]
     topLevelIssues.each do |issue|   
       relatedIDs = issue.self_and_descendants.pluck(:id).to_a() # get own and all related issue IDs
-      relatedTimeEntries = timeEntries.where(:issue_id => relatedIDs).to_a()
-      
-      logger.info "related time entries for issue "+issue.id.to_s()+" found:"+relatedTimeEntries.count().to_s()
-      if (relatedTimeEntries.count() > 0)
-        logger.info relatedTimeEntries
+      relatedTimeEntries = timeEntries.where(:issue_id => relatedIDs)
+      relatedTimeEntries.each do |timeEntry|
+        @time_entries.push({'topLevelID'=>issue.id, 'issueID'=>timeEntry.issue_id, 'userID'=>timeEntry.user_id, 'hours'=>timeEntry.hours, 'comments'=>timeEntry.comments, 'spentOn'=>timeEntry.spent_on})
       end
-      @issues.push({'id'=>issue.id, 'subject'=> issue.subject, 'issue' => issue, 'timeEntries'=>relatedTimeEntries})
-      #puts relatedTimeEntries.count()
     end
 
-    @time_entries = timeEntries.to_a()
+    # read issues
+    issues = Issue.where( :project_id => @project.id).to_a()
+    @issues = issues.map{ |issue| {'issueID'=>issue.id, 'subject'=> issue.subject, 'tracker' => issue.tracker, 'startDate'=>issue.start_date, 'dueDate'=>issue.due_date, 'estimatedHours'=>issue.estimated_hours, 'closedOn'=>issue.closed_on}}
+
 
   end
 end
